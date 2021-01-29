811148b4f86773b87a73b455dd05bca7cfa570f3
Milica Bulajic <milica.bulajic@gmail.com>
2021-01-29 21:20:40 +0100

    built login page

---
+++ src/components/Login/Login.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div>
+      <p>Please Log In</p>
+      <form>
+        <label>
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
