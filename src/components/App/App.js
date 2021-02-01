7357e7cdb7f7062d95ded6d59479cefd5fc5bedd
Milica Bulajic <milica.bulajic@gmail.com>
2021-02-01 17:26:53 +0100

    Firebase setup, signup component & auth context setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,15 @@
+import { AuthProvider } from "components/Auth/AuthContext";
+import Signup from "components/Auth/Signup";
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Dashboard from "../Dashboard/Dashboard";
-import Preferences from "../Preferences/Preferences";
-import Login from "../Login/Login.js";
-import useToken from "./useToken";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const { token, setToken } = useToken();
-
-  if (!token) {
-    return <Login setToken={setToken} />;
-  }
-
   return (
-    <div className="wrapper">
-      <h1>Application - personal growth</h1>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Signup />
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
