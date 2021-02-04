e85c34d020ebfa8029b121f181655f54c2df8da3
Milica Bulajic <milica.bulajic@gmail.com>
2021-02-04 12:36:31 +0100

    material ui module installing & set ref into inputRef

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -2,7 +2,37 @@ import React, { useRef, useState } from "react";
 import { useAuth } from "components/Auth/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Alert } from "@material-ui/lab";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 export default function Login() {
+  const classes = useStyles();
   const emailRef = useRef();
   const passwordRef = useRef();
   const { login } = useAuth();
@@ -10,7 +40,7 @@ export default function Login() {
   const [loading, setLoading] = useState(false);
   const history = useHistory();
 
-  async function handleSubmit(e) {
+  async function onSubmit(e) {
     e.preventDefault();
 
     try {
@@ -26,30 +56,55 @@ export default function Login() {
   }
 
   return (
-    <>
-      <h2 className="text-center mb-4">Log In</h2>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div id="email">
-          <p>Email</p>
-          <input name="emailInput" type="email" ref={emailRef} required />
-        </div>
-        <div id="password">
-          <p>Password</p>
-          <input
-            name="passwordInput"
-            type="password"
-            ref={passwordRef}
-            required
-          />
-        </div>
-        <button disabled={loading} className="w-100" type="submit">
-          Log in
-        </button>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Typography component="h1" variant="h5">
+        Login
+      </Typography>
+      {error && <Alert severity="error">{error}</Alert>}
+      <form className={classes.form} onSubmit={(e) => onSubmit(e)}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              inputRef={emailRef}
+              required
+              fullWidth
+              type="email"
+              id="email"
+              label="Email Address"
+              name="email"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              inputRef={passwordRef}
+              required
+              fullWidth
+              type="password"
+              id="password"
+              label="Password"
+              name="passwordInput"
+            />
+          </Grid>
+          <Button
+            disabled={loading}
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Log in
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link to="/signup" href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Grid>
       </form>
-      <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Create Account</Link>
-      </div>
-    </>
+    </Container>
   );
 }
